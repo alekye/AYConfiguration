2c34b5aca1ec46117f7b7534fd925a0c08989990
Alek Ye <alek.ye@meehealth.com>
2018-05-12 16:10:51 +0800

    add vimrc file

---
+++ .vimrc
@@ -0,0 +1,5 @@
+set number	# 显示行号
+set tabstop=4	# tab代表4个空格
+set autoindent	# 自动缩进
+syn on	# 语法高亮
+set encoding=utf-8	# 编码为utf8
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# auto pull the configure file
+
+# .vimrc
+curl -o ~/.vimrc https://github.com/alekye/AYConfiguration/blob/master/.vimrc
